b4aadb84f34dbc1eaba0eb1f663e68bb6b329dde
Ellen Felícia Ferreira Miranda <ellen.miranda@edu.unimontes.br>
2025-04-11 12:56:41 -0300

    feat: Implement pagination for job listings and enhance email templates
    - Added pagination to the job listings page, displaying 5 jobs per page.
    - Updated the AppServiceProvider to prepare for Bootstrap 5 pagination.
    - Created custom Blade components for email layout, header, footer, button, and other elements.
    - Added CSS styles for email templates to ensure proper formatting across devices.
    - Introduced new pagination views for Bootstrap 4, Bootstrap 5, and Tailwind CSS.
    - Enhanced the job creation route to include validation for employer_id, title, and salary.

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,18 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->paginate(5);
     return view('jobs',[
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-$job = Job::find($id);
-
-return view('job', ['job' => $job]);
-
+Route::post('/jobs', function () {
+    request()->validate([
+        'employer_id' => ['required', 'exists:employers'],
+        'title'       => ['required'],
+        'salary'      => ['required'],
+    ]);
 });
 
 Route::get('/contact', function () {
