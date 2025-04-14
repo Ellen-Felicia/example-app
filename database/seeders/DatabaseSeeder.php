a27c2640bd63cef768062ae85c213f318eb4aefc
Ellen Felícia Ferreira Miranda <ellen.miranda@edu.unimontes.br>
2025-04-14 12:15:08 -0300

    feat: Add job creation and display features, including seeder and routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Job;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' =>' Test',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
+
     }
 }
